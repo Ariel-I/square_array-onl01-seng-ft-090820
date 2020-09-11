3fe5f4952ff5ed6002080890ed3fd7a2e90ba284
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-11 19:38:22 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,10 @@
 array = [1,2,3]
 
+
+
 def square_array(array)
-  array.each do |number|
- "#{array}"** 2  
-end
+  Enumerator.new do |number|
+    array.each { |e| number << e ** 2 }    
+  end
+  .take(array.length)
 end
