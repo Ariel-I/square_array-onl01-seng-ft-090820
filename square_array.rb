63609607a0b12b5759b6bbcf934e3615ae442532
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-11 19:41:27 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ array = [1,2,3]
 
 def square_array(array)
   Enumerator.new do |number|
-    array.each { |array| number << e ** 2}
+    array.each { |array| number << array ** 2}
   end
   .take(array.length)
 end
